185716d558dc593dbc5a7b7f976ecd4df35c3f90
Aleytia <jarek13705@gmail.com>
2023-03-04 01:46:52 +0900

    Use OpenGraph instead of direct APIs

---
+++ rss/shared/beautifulsoup_service.py
@@ -0,0 +1,84 @@
+
+import requests
+
+from bs4 import BeautifulSoup
+from retry import retry
+
+SPOOF_DISCORD_USER_AGENT = "Mozilla/5.0 (compatible; Discordbot/2.0; +https://discordapp.com)"
+
+
+class Metadata:
+
+    def __init__(self,
+                 description: str = None,
+                 image: str = None,
+                 site_name: str = None,
+                 title: str = None,
+                 url: str = None):
+
+        self._description = description
+        self._image = image
+        self._site_name = site_name
+        self._title = title
+        self._url = url
+
+    @property
+    def description(self):
+        return self._description
+
+    @property
+    def image(self):
+        return self._image
+
+    @property
+    def site_name(self):
+        return self._site_name
+
+    @property
+    def title(self):
+        return self._title
+
+    @property
+    def url(self):
+        return self._url
+
+
+def fetch_page_metadata(url: str, spoof_user_agent: bool = True):
+    """
+    Fetches OpenGraph metadata from websites for rendering in Discord embeds.
+    """
+
+    @retry(tries=3, delay=2)
+    def fetch(url: str, session: requests.Session):
+        return session.get(url)
+
+    def get_og_meta_content(soup: BeautifulSoup, property: str):
+        meta_property = soup.find("meta", property=property)
+        return meta_property["content"]
+
+    session = requests.Session()
+    if spoof_user_agent:
+        session.headers.update({'User-Agent': SPOOF_DISCORD_USER_AGENT})
+
+    page = fetch(url, session)
+    if page.status_code != 200:
+        return None
+
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    # Start to fetch metadata
+    og_description = get_og_meta_content(soup, "og:description")
+    og_image = get_og_meta_content(soup, "og:image:secure_url")
+    if not og_image:
+        og_image = get_og_meta_content(soup, "og:image")
+    og_site_name = get_og_meta_content(soup, "og:site_name")
+    og_title = get_og_meta_content(soup, "og:title")
+    og_url = get_og_meta_content(soup, "og:url")
+
+    return Metadata(
+        description=og_description,
+        image=og_image,
+        site_name=og_site_name,
+        title=og_title,
+        url=og_url
+    )
